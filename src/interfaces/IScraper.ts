7b709b4faf2ef28a574b6f531b4cd98734464572
jeanpierrebroz <jeanpierrebroz@gmail.com>
2025-06-07 15:07:11 -0600

    working more on search features

--- src/interfaces/IScraper.ts
+++ src/interfaces/IScraper.ts
@@ -2,9 +2,5 @@ import {Browser, BrowserContext, Page} from "playwright";
 
 export interface IScraper
 {
-    browser: Browser;
-    context: BrowserContext;
-    page: Page;
-
-    search(query: string): Promise<string>;
+    search(query: string): Promise<string[]>;
 }
--- src/scraper/scraper.ts
+++ src/scraper/scraper.ts
@@ -1,30 +1,52 @@
-import {Browser, chromium, devices, BrowserContext, Page} from "playwright";
+import {Browser, chromium, devices, BrowserContext, Page, Response} from "playwright";
 import { IScraper } from "../interfaces/IScraper"
 
 export class Scraper implements IScraper
 {
-    browser!: Browser;
-    context!: BrowserContext;
-    page!: Page;
-    constructor() {}
+    private browser: Browser;
+    private context: BrowserContext;
+    private page: Page;
+    private constructor(browser: Browser, context: BrowserContext, page: Page) {
+        this.browser = browser;
+        this.context = context;
+        this.page = page;
+    }
 
     static async setup(): Promise<Scraper>
     {
-        const scraper = new Scraper();
 
-        scraper.browser = await chromium.launch();
-        scraper.context = await scraper.browser.newContext(devices['iPhone 14 Pro'])
-        scraper.page = await scraper.context.newPage();
+        const browser = await chromium.launch();
+        const context = await browser.newContext(devices['iPhone 14 Pro'])
+        const page = await context.newPage();
 
-        return scraper;
+        return new Scraper(browser, context, page);
     }
 
-
-
-    async search(searchTerm: string): Promise<string>
+    //Called for each keyword phrase
+    async search(searchTerm: string): Promise<string[]>
     {
+        var result: string[] = [];
+        var searchResults: Response | null = await this.getWebsiteResults(searchTerm);
+        if (searchResults == null)
+        {
+            return [];
+        }
 
-        return '';
+        return result;
+    }
+
+    async close(): Promise<void>
+    {
+        await this.browser.close();
+        await this.page.close();
+        await this.browser.close();
+    }
+
+    private async getWebsiteResults(searchTerm: string): Promise<Response | null>
+    {
+        var url: string = "https://www.bing.com/search?q=" + searchTerm;
+        var response = await this.page.goto(url);
+        return response;
     }
 
 }
